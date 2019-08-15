00e0ac122367ae6391dfe2ec2967a270891e3c97
henry_lin@umc.com <00002882>
2019-08-15 16:06:10 +0800

    0.1

---
+++ __init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'vector2',
+    'vector3',
+    'util',
+    'sphere',
+    'matrix44',
+    'color',
+    'gametime',
+    'grid'
+]
+
+__version__ = "0.0.3"
---
+++ locals.py
@@ -0,0 +1,4 @@
+(WRAP_REPEAT,
+ WRAP_CLAMP,
+ WRAP_ERROR,
+ WRAP_NONE) = range(4)
